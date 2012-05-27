ba5af9dd583a265d4f96a59ac64f94b35ed7fffe
edi.ermawan <edi.ermawan@gmail.com>
2012-05-27 17:21:57 +0000

    -add dir out src setting.

--- Xedi/Xedithor/src/appconfig.cpp
+++ Xedi/Xedithor/src/appconfig.cpp
@@ -11,13 +11,20 @@ AppConfig::AppConfig()
     m_setting->beginGroup("WorkingDirectory");
     Config.workingDir = m_setting->value("workingDir","").toString();
     Config.exportDir  = m_setting->value("exportDir","").toString();
+    Config.exportDirSrc  = m_setting->value("exportDirSrc","").toString();
+    Config.package       = m_setting->value("package","").toString();
 
     if(Config.workingDir =="")
     {
         m_setting->setValue("workingDir",QApplication::applicationDirPath());
         m_setting->setValue("exportDir",QApplication::applicationDirPath());
-        Config.workingDir =QApplication::applicationDirPath();
-        Config.exportDir  =QApplication::applicationDirPath();
+        m_setting->setValue("exportDirSrc",QApplication::applicationDirPath());
+        m_setting->setValue("package","");
+
+        Config.workingDir    = QApplication::applicationDirPath();
+        Config.exportDir     = QApplication::applicationDirPath();
+        Config.exportDirSrc  = QApplication::applicationDirPath();
+        Config.package       = "";
     }
     m_setting->endGroup();
 
@@ -36,12 +43,14 @@ void AppConfig::deleteInstance()
     m_Instance = 0;
 }
 
-void AppConfig::saveWorkingDir(QString wDir,QString eDir)
+void AppConfig::saveWorkingDir(QString wDir,QString eDir,QString eDirSrc,QString package)
 {
     m_setting->beginGroup("WorkingDirectory");
 
     m_setting->setValue("workingDir",wDir);
     m_setting->setValue("exportDir",eDir);
+    m_setting->setValue("exportDirSrc",eDirSrc);
+    m_setting->setValue("package",package);
 
     m_setting->endGroup();
 }
